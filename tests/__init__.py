8656fd7aa4503f759fd242ab532c4fc59158f1eb
Vašek Dohnal <vaclav.dohnal@gmail.com>
2022-07-26 22:08:01 +0200

    Add ignore mypy check

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,5 +11,5 @@ logger: Final = logging.getLogger(__name__)
 
 
 if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # type: ignore
     logger.info(f"Installed `{asyncio.WindowsSelectorEventLoopPolicy!r}` as a loop policy.")
