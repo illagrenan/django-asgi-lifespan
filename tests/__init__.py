61d379aa354f8f2d9ca94a798fe36828819a0245
Vašek Dohnal <vaclav.dohnal@gmail.com>
2022-07-26 22:23:33 +0200

    Fix mypy errors

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,5 +10,5 @@ from typing import Final
 logger: Final = logging.getLogger(__name__)
 
 if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
-    logger.info(f"Installed `{asyncio.WindowsSelectorEventLoopPolicy!r}` as a loop policy.")  # type: ignore
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    logger.info("Installed `%r` as a loop policy.", asyncio.WindowsSelectorEventLoopPolicy)
