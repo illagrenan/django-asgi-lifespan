352bdad30590f782478229e93d8f751737359940
Vašek Dohnal <vaclav.dohnal@gmail.com>
2022-07-26 22:35:18 +0200

    Use sys.platform to check for Windows

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,13 +2,12 @@
 
 import asyncio
 import logging
-import os
 import sys
 
 from typing import Final
 
 logger: Final = logging.getLogger(__name__)
 
-if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and os.name == "nt":
+if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
     logger.info("Installed `%r` as a loop policy.", asyncio.WindowsSelectorEventLoopPolicy)  # type: ignore
