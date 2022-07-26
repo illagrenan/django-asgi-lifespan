f8eec517fc0ea4d591c12c9bb5dd5bb787303af6
Vašek Dohnal <vaclav.dohnal@gmail.com>
2022-07-26 22:12:48 +0200

    Fix mypy errors

--- tests/__init__.py
+++ tests/__init__.py
@@ -11,4 +11,4 @@ logger: Final = logging.getLogger(__name__)
 
 if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
-    logger.info(f"Installed `{asyncio.WindowsSelectorEventLoopPolicy!r}` as a loop policy.")
+    logger.info(f"Installed `{asyncio.WindowsSelectorEventLoopPolicy!r}` as a loop policy.")  # type: ignore
