b2f5fd25afe6d4cb6240920cc47c3f63a90c0119
Vašek Dohnal <vaclav.dohnal@gmail.com>
2022-07-26 22:08:11 +0200

    Formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,7 +9,6 @@ from typing import Final
 
 logger: Final = logging.getLogger(__name__)
 
-
 if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # type: ignore
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # type: ignore
     logger.info(f"Installed `{asyncio.WindowsSelectorEventLoopPolicy!r}` as a loop policy.")
