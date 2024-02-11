f910dc71a712bb6c39c86b32f49509d9d638122b
Vašek Dohnal <vaclav.dohnal@gmail.com>
2024-02-11 21:28:37 +0100

    Add middleware

---
+++ tests/django_test_application/test_app/middleware.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+# ! python3
+
+import logging
+from typing import Final, Callable, Awaitable
+
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
+from django.core.handlers.asgi import ASGIRequest
+from django.http import HttpRequest, HttpResponseBase
+
+logger: Final = logging.getLogger(__name__)
+
+
+class AsyncMiddleware:
+    async_capable = True
+    sync_capable = False
+
+    def __init__(
+        self, get_response: Callable[[HttpRequest], Awaitable[HttpResponseBase]]
+    ) -> None:
+        self.get_response = get_response
+
+        if iscoroutinefunction(self.get_response):
+            markcoroutinefunction(self)
+
+    async def __call__(self, request: ASGIRequest) -> HttpResponseBase:
+        setattr(request, "state", request.scope["state"])
+        return await self.get_response(request)
