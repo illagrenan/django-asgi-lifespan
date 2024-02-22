7020b22279391a0703e5f09aef25749682350028
Vašek Dohnal <vaclav.dohnal@gmail.com>
2024-02-22 10:37:00 +0100

    Add manage.py for testing runserver

---
+++ tests/django_test_application/manage.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# ! python3
+
+import os
+import sys
+from pathlib import Path
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_test_application.settings")
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django  # noqa
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+
+        raise
+
+    # This adds tests directory to the PYTHONPATH
+    current_path = Path(__file__).parent.parent.resolve()
+    sys.path.append(str(current_path))
+
+    execute_from_command_line(sys.argv)
