efdbd2069e8066b8eeef06b56c5b80efa99f3330
Václav Dohnal <vaclav.dohnal@gmail.com>
2024-09-21 22:36:31 +0200

    Maintenance (#102)

--- example/context.py
+++ example/context.py
@@ -7,9 +7,7 @@ from django_asgi_lifespan.types import State
 
 @asynccontextmanager
 async def httpx_lifespan_manager() -> State:
-    state = {
-        "httpx_client": httpx.AsyncClient()
-    }
+    state = {"httpx_client": httpx.AsyncClient()}
 
     try:
         yield state
